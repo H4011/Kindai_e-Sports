888001b5697b0a88adebcae56a23975673300e5c
Macaron <72959185+H4011@users.noreply.github.com>
2022-04-27 10:51:03 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-  var instance = M.Tabs.init(el, options);
+var ua = window.navigator.userAgent.toLowerCase();
 
-  // Or with jQuery
+if(ua.indexOf("windows nt") !== -1) {
+  console.log("「Microsoft Windows」をお使いですね!");
+} else if(ua.indexOf("android") !== -1) {
+  console.log("「Android」をお使いですね!");
+} else if(ua.indexOf("iphone") !== -1 || ua.indexOf("ipad") !== -1) {
+  console.log("「iOS」をお使いですね!");
+} else if(ua.indexOf("mac os x") !== -1) {
+  console.log("「macOS」をお使いですね!");
+} else {
+  console.log("何をお使いなのですか?");
+}
 
-  $(document).ready(function(){
-    $('.tabs').tabs();
-  });
